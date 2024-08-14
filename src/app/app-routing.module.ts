4a43e55c5f2aefd97d2103faac924d7aeb4e5982
Chaitu <chaitanyakunchalao7@gmail.com>
2024-08-14 12:10:58 +0530

    changed home button style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { ResumeComponent } from './resume/resume.component'; // Import ResumeComponent
 import { SkillsComponent } from './skills/skills.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
   { path: 'projects', component: ProjectsComponent },
+  { path: 'skills', component: SkillsComponent },
   { path: 'resume', component: ResumeComponent },
-  {path: 'skills', component:SkillsComponent} // Define route for ResumeComponent
-  // Add more routes here as needed
+  { path: 'contact', component: ContactComponent },
+  //wildcard route for 404 page
+  { path: '', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
